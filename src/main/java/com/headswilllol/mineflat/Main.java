2bf6e1e8598440adec92aae24d73e61223275542
mproncace <mproncace@gmail.com>
2014-12-27 20:53:52 -0500

    Add license headers to source files

--- src/main/java/com/headswilllol/mineflat/Main.java
+++ src/main/java/com/headswilllol/mineflat/Main.java
@@ -1,6 +1,6 @@
 /*
  * MineFlat
- * Copyright (c) 2014, Maxim Roncacé <http://github.com/mproncace>
+ * Copyright (c) 2014, Maxim Roncacé <mproncace@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
